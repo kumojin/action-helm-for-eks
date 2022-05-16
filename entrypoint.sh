2e1c5c2b256355738dbe706aaacc46ffbda33e0c
Julien Maitrehenry <julien@kumojin.com>
2022-05-16 12:55:44 -0400

    Enforce HELM_PLUGINS directory
    Github Action will change the $HOME env var and the plugins are inside the $HOME directory

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,4 +3,6 @@ set -e
 
 aws eks update-kubeconfig --name "$CLUSTER_NAME" --region "$AWS_REGION" --kubeconfig ~/.kube/config
 
+# Github Action will change the $HOME env var and the plugins are inside the $HOME directory
+export HELM_PLUGINS=/root/.local/share/helm/plugins
 helm "$@"
